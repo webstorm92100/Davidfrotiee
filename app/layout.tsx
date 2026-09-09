a2b43eda6ba051f58d4781a321d70ce07ce8bbb7
Marc Signorel <marc@M5-pro-M.local>
2026-09-09 11:45:15 +0200

    Ajouter Google Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -48,6 +48,22 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="fr">
+      <head>
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-6SQT165TYX"
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-6SQT165TYX');
+            `,
+          }}
+        />
+      </head>
       <body>
         {children}
         <script
